a5e6b8650608991382eed058960b54afc5e03d76
Samiksha2021 <samikshasuryawanshi25@gmail.com>
2023-12-13 18:36:50 +0530

    update

--- src/ViewFood.jsx
+++ src/ViewFood.jsx
@@ -3,6 +3,26 @@ import arr from './CardData'
 const ViewFood = () => {
 
     const zomatologo='https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png'
+    const [data,SetData]=useState()
+    const [search,SetSearch]=useState(arr)
+
+    const fun1=(e)=>{
+       SetData(e.target.value)
+      // console.log(e.target.value,'rrr');
+    }
+
+
+    const fun2=()=>{
+      let newR=arr.filter((str)=>{
+        return str.text===data
+      })
+      SetSearch(newR)
+    }
+
+    
+
+  
+    
   return (
     <>
     <div className='container w-75 d-flex justify-content-between align-items-center'>
@@ -14,10 +34,10 @@ const ViewFood = () => {
     <form class='container d-flex justify-content-center align-items-center m-2'>
 
     <div class="col-lg-4">
-    <input type='text' class="form-control" id=" inputPassword2" placeholder='Search'/>    
+    <input name='data' value={data} onChange={fun1} type='text' class="form-control" id=" inputPassword2" placeholder='Search'/>    
     </div>
     <div class="col-auto mx-2">
-        <button type="submit" class="btn btn-primary">Search </button>
+        <button  onClick={fun2} type="submit" class="btn btn-primary">Search </button>
     </div>
     </form>
     </div>
@@ -26,7 +46,7 @@ const ViewFood = () => {
       <h2 style={{fontWeight:400}} className=''>Best food in Bhopal</h2>
       <div className='row  d-flex justify-content-between align-items-center'>
         {
-          arr.map((res)=>{
+          search.map((res)=>{
             return(<>
             <div class="card mt-4" style={{width: 300}}>
               <img src={res.img} class="card-img-top" alt="..."/>
@@ -38,21 +58,12 @@ const ViewFood = () => {
             </>)
           })
         }
-
-     
-
-
-
-
       </div>
     </section>
-    
-
-
-    
     </>
-    
+     
   )
+
 }
 
 export default ViewFood
